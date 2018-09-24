bf37e8c40110ae146db3e1c1a80c06273c7677a0
Jeff Campbell <jeffdcamp@gmail.com>
2018-09-23 23:36:25 -0600

    MinSdk 17 Cleanup

--- app/src/main/kotlin/org/dbtools/sample/work/ux/WorkManagerMonitorActivity.kt
+++ app/src/main/kotlin/org/dbtools/sample/work/ux/WorkManagerMonitorActivity.kt
@@ -14,17 +14,5 @@ class WorkManagerMonitorActivity : AppCompatActivity() {
 
 
         supportFragmentManager.beginTransaction().replace(R.id.fragmentPosition1, WorkManagerStatusFragment()).commit()
-
-
-//        binding.monitorButton.setOnClickListener { showWorkManagerMonitor() }
-//        binding.updateButton.setOnClickListener { updateLastIndividual() }
-//        binding.deleteButton.setOnClickListener { deleteLastIndividual() }
-//        binding.showButton.setOnClickListener { showLastIndividualName() }
-//        binding.testDatabaseRepositoryButton.setOnClickListener { testDatabaseRepository() }
-//        binding.testRoomLiveDataButton.setOnClickListener { testRoomLiveData() }
-//        binding.testMergeDatabaseButton.setOnClickListener { testMergeDatabase() }
-//        binding.logIndividualsButton.setOnClickListener { individualRepository.showAllIndividuals() }
-//        binding.deleteAllIndividualsButton.setOnClickListener { individualRepository.deleteAllIndividuals() }
-//        binding.validateDatabaseButton.setOnClickListener { testValidateDatabase() }
     }
 }
